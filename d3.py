754390dee6f66857cbb74798173eacfa7be65446
yuki23329626 <michael7105@gmail.com>
2020-04-05 00:02:00 +0800

    add existing files

---
+++ d3.py
@@ -0,0 +1,36 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+mu, sigma = 0, 1 # mean and standard deviation
+s = np.random.normal(mu, sigma, 20) #Create Gaussian Noise
+x = np.linspace(-3, 3, 20)
+y = 2*x+s
+baseLine = 2*x+0
+
+#Plot Gaussian Noise
+plt.subplot(1,1,1);
+plt.xlim(-3,3)
+plt.ylim(-15,30)
+plt.plot(x, y, 'ro', x, baseLine)
+
+plt.show()
+
+# create dummy data for training
+x_values = [i for i in range(11)]
+x_train = np.array(x_values, dtype=np.float32)
+x_train = x_train.reshape(-1, 1)
+
+y_values = [2*i + 1 for i in x_values]
+y_train = np.array(y_values, dtype=np.float32)
+y_train = y_train.reshape(-1, 1)
+
+
+
+##Plot Gaussian Distribution
+#plt.subplot(1,2,2);
+#count, bins, ignored = plt.hist(s, 300, normed=True)
+#plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * 
+#         np.exp( - (bins - mu)**2 / (2 * sigma**2) ), 
+#         linewidth=2, color='r')
+#plt.show()
---
+++ demo.py
@@ -0,0 +1,9 @@
+import numpy as np
+# create dummy data for training
+x_values = [i for i in range(11)]
+x_train = np.array(x_values, dtype=np.float32)
+x_train = x_train.reshape(-1, 1)
+
+y_values = [2*i + 1 for i in x_values]
+y_train = np.array(y_values, dtype=np.float32)
+y_train = y_train.reshape(-1, 1)
