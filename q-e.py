6ad023266544ef14729d34e6fc0a5ada7a800652
yukine23329626 <michael7105@gmail.com>
2020-04-07 19:16:04 +0800

    test

--- q-e.py
+++ q-e.py
@@ -10,7 +10,7 @@ def linear_regression ( x_training, y_training, x_valid, y_valid, sum_training, 
   return  wlin
 
 def ridge_regression ( x_training, y_training, x_valid, y_valid, sum_training, sum_valid, lamda):
-  wlin = np.linalg.inv(x_training.T.dot( x_training)).dot(x_training.T.dot(y_training)+lamda*np.eye(x_training[0]))
+  wlin = np.linalg.inv(x_training.T.dot( x_training)+lamda*np.eye(x_training.shape[0])).dot(x_training.T.dot(y_training))
   return  wlin
 
 def calculate_error_L1 ( x_training, y_training, x_valid, y_valid, sum_training, sum_valid):
@@ -19,8 +19,8 @@ def calculate_error_L1 ( x_training, y_training, x_valid, y_valid, sum_training,
     error_testing = abs((x_valid.dot(wlin) - y_valid).sum()/sum_valid)
     return  wlin, error_training, error_testing
 
-def calculate_error_L2 ( x_training, y_training, x_valid, y_valid, sum_training, sum_valid):
-    wlin = ridge_regression( x_training, y_training, x_valid, y_valid, sum_training, sum_valid)
+def calculate_error_L2 ( x_training, y_training, x_valid, y_valid, sum_training, sum_valid, lamda):
+    wlin = ridge_regression( x_training, y_training, x_valid, y_valid, sum_training, sum_valid, lamda)
     error_training = abs((x_training.dot(wlin) - y_training).sum()/sum_training)
     error_testing = abs((x_valid.dot(wlin) - y_valid).sum()/sum_valid)
     return  wlin, error_training, error_testing
@@ -51,8 +51,9 @@ def generator_x (x_training, x_testing, p_st, p_end, s_trn, s_tst):
        x_testing = np.hstack(((x_testing[:,-2]**i).reshape(s_tst,1) ,x_testing))
     return x_training , x_testing
 
-size = 60
-x = np.linspace(-3, 3, size)
+size = 20
+lamda = 0
+x = np.linspace(0, 1, size)
 y = np.zeros((size,1))
 
 mu, sigma = 0, 1 # mean and standard deviation
@@ -67,16 +68,16 @@ x_training, x_testing, y_training, y_test = train_test_split(x, y, test_size=0.2
 wlin, error_training, error_testing = calculate_error_L1 (x_training, y_training, x_testing, y_test, x_training.shape[0], x_testing.shape[0])
 
 #### use to plot the fucntion
-x_line = np.linspace (-3, 3 ,2000)
+x_line = np.linspace (0, 1 ,2000)
 y_line = x_line * wlin[0] + wlin[1]
 
 fig, ax = plt.subplots()
 
-print ("\nQuestion d m = 60:" )
+print ("\nQuestion d λ=0:" )
 
 x_training_degree14 ,x_testing_degree14 = generator_x (x_training, x_testing, 1 , 14, x_training.shape[0], x_testing.shape[0])
-wlin_degree14, error_training_degree14, error_testing_degree14 = calculate_error_L1 (x_training_degree14, y_training, x_testing_degree14, y_test, x_training.shape[0], x_testing.shape[0])
-print ("\n--Degree 5--")
+wlin_degree14, error_training_degree14, error_testing_degree14 = calculate_error_L2 (x_training_degree14, y_training, x_testing_degree14, y_test, x_training.shape[0], x_testing.shape[0], lamda)
+print ("\n--Degree 14--")
 print ("Training error: " , error_training_degree14)
 print ("Leave One out: " ,leave_one_out(x_training_degree14, y_training))
 print ("Five Fold Error: " ,five_fold(x_training_degree14, y_training))
@@ -86,8 +87,10 @@ y_degree_m1=0
 for i in range (0, 15):
     y_degree_m1 += x_line**(14-i)* wlin_degree14 [i]
 
-size = 160
-x = np.linspace(-3, 3, size)
+print ("\nQuestion d λ=0.001/m:" )
+
+lamda = 0.001/size
+x = np.linspace(0, 1, size)
 y = np.zeros((size,1))
 
 mu, sigma = 0, 1 # mean and standard deviation
@@ -99,8 +102,8 @@ x = np.reshape( np.append( x, np.ones(size)), (2, size)).T
 x_training, x_testing, y_training, y_test = train_test_split(x, y, test_size=0.25)
 
 x_training_degree14 ,x_testing_degree14 = generator_x (x_training, x_testing, 1 , 14, x_training.shape[0], x_testing.shape[0])
-wlin_degree14, error_training_degree14, error_testing_degree14 = calculate_error_L1 (x_training_degree14, y_training, x_testing_degree14, y_test, x_training.shape[0], x_testing.shape[0])
-print ("\n--Degree 5--")
+wlin_degree14, error_training_degree14, error_testing_degree14 = calculate_error_L2 (x_training_degree14, y_training, x_testing_degree14, y_test, x_training.shape[0], x_testing.shape[0], lamda)
+print ("\n--Degree 14--")
 print ("Training error: " , error_training_degree14)
 print ("Leave One out: " ,leave_one_out(x_training_degree14, y_training))
 print ("Five Fold Error: " ,five_fold(x_training_degree14, y_training))
@@ -110,9 +113,10 @@ y_degree_m2=0
 for i in range (0, 15):
     y_degree_m2 += x_line**(14-i)* wlin_degree14 [i]
 
+print ("\nQuestion d λ=1/m:" )
 
-size = 320
-x = np.linspace(-3, 3, size)
+lamda = 1/size
+x = np.linspace(0, 1, size)
 y = np.zeros((size,1))
 
 mu, sigma = 0, 1 # mean and standard deviation
@@ -124,8 +128,8 @@ x = np.reshape( np.append( x, np.ones(size)), (2, size)).T
 x_training, x_testing, y_training, y_test = train_test_split(x, y, test_size=0.25)
 
 x_training_degree14 ,x_testing_degree14 = generator_x (x_training, x_testing, 1 , 14, x_training.shape[0], x_testing.shape[0])
-wlin_degree14, error_training_degree14, error_testing_degree14 = calculate_error_L1 (x_training_degree14, y_training, x_testing_degree14, y_test, x_training.shape[0], x_testing.shape[0])
-print ("\n--Degree 5--")
+wlin_degree14, error_training_degree14, error_testing_degree14 = calculate_error_L2 (x_training_degree14, y_training, x_testing_degree14, y_test, x_training.shape[0], x_testing.shape[0], lamda)
+print ("\n--Degree 14--")
 print ("Training error: " , error_training_degree14)
 print ("Leave One out: " ,leave_one_out(x_training_degree14, y_training))
 print ("Five Fold Error: " ,five_fold(x_training_degree14, y_training))
@@ -135,9 +139,37 @@ y_degree_m3=0
 for i in range (0, 15):
     y_degree_m3 += x_line**(14-i)* wlin_degree14 [i]
 
-ax.plot (x_line, y_degree_m1, 'r', label='m = 60' )
-ax.plot (x_line, y_degree_m2, 'g', label='m = 160' )
-ax.plot (x_line, y_degree_m3, 'b', label='m = 320' )
+print ("\nQuestion d λ=1000/m:" )
+
+lamda = 1000/size
+x = np.linspace(0, 1, size)
+y = np.zeros((size,1))
+
+mu, sigma = 0, 1 # mean and standard deviation
+noise = np.random.normal(mu, sigma, size) #Create Gaussian Noise
+
+y = 2*x + noise
+
+x = np.reshape( np.append( x, np.ones(size)), (2, size)).T
+x_training, x_testing, y_training, y_test = train_test_split(x, y, test_size=0.25)
+
+x_training_degree14 ,x_testing_degree14 = generator_x (x_training, x_testing, 1 , 14, x_training.shape[0], x_testing.shape[0])
+wlin_degree14, error_training_degree14, error_testing_degree14 = calculate_error_L2 (x_training_degree14, y_training, x_testing_degree14, y_test, x_training.shape[0], x_testing.shape[0], lamda)
+print ("\n--Degree 14--")
+print ("Training error: " , error_training_degree14)
+print ("Leave One out: " ,leave_one_out(x_training_degree14, y_training))
+print ("Five Fold Error: " ,five_fold(x_training_degree14, y_training))
+print ("Testing error: " , error_training_degree14)
+
+y_degree_m4=0
+for i in range (0, 15):
+    y_degree_m4 += x_line**(14-i)* wlin_degree14 [i]
+
+
+ax.plot (x_line, y_degree_m1, 'r', label='λ=0' )
+ax.plot (x_line, y_degree_m2, 'g', label='λ=0.001/m' )
+ax.plot (x_line, y_degree_m3, 'b', label='λ=1/m' )
+ax.plot (x_line, y_degree_m3, 'k', label='λ=1000/m' )
 
 legend = ax.legend(loc='upper left', shadow=True, fontsize='x-large')
 
