b85e294f253d13c2ce671435230644596b83ef89
yuki23329626 <michael7105@gmail.com>
2020-06-09 02:28:06 +0800

    test

--- 3-hw/celebA_data_preprocess.py
+++ 3-hw/celebA_data_preprocess.py
@@ -4,13 +4,13 @@ from scipy.misc import imresize
 
 # root path depends on your computer
 root = '/home/nxshen/machine-learning/3-hw/data/'
-save_root = '/home/nxshen/machine-learning/3-hw/resized_celebA/'
+save_root = '/home/nxshen/machine-learning/3-hw/'
 resize_size = 64
 
 if not os.path.isdir(save_root):
     os.mkdir(save_root)
-if not os.path.isdir(save_root + 'celebA'):
-    os.mkdir(save_root + 'celebA')
+if not os.path.isdir(save_root + 'resized_celebA'):
+    os.mkdir(save_root + 'resized_celebA')
 img_list = os.listdir(root)
 
 # ten_percent = len(img_list) // 10
@@ -18,7 +18,7 @@ img_list = os.listdir(root)
 for i in range(len(img_list)):
     img = plt.imread(root + img_list[i])
     img = imresize(img, (resize_size, resize_size))
-    plt.imsave(fname=save_root + 'celebA/' + img_list[i], arr=img)
+    plt.imsave(fname=save_root + 'resized_celebA/' + img_list[i], arr=img)
 
     if (i % 1000) == 0:
         print('%d images complete' % i)
