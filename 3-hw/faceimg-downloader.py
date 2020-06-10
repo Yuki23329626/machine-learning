4d180c4a7ba0b004cc90b47bb9a1f4e39ff8c65e
yuki23329626 <michael7105@gmail.com>
2020-06-10 14:45:19 +0800

    test

--- 3-hw/faceimg-downloader.py
+++ 3-hw/faceimg-downloader.py
@@ -1,4 +1,3 @@
-text_file = open("pubfig.txt", "r")
-lines = text_file.readlines()
-print(lines[0])
-text_file.close()
+from numpy import loadtxt
+lines = loadtxt("pubfig.txt", comments="#", delimiter="\t", unpack=False)
+print(lines)
