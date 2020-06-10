127cff6fe82a15bf0500aacd04e751389b7b1009
yuki23329626 <michael7105@gmail.com>
2020-06-10 15:06:21 +0800

    test

--- 3-hw/faceimg-downloader.py
+++ 3-hw/faceimg-downloader.py
@@ -13,7 +13,7 @@ with open('pubfig.txt') as object_pubfig:
       li=line.strip()
       if not li.startswith("#"):
         #print(line.split("\t")[2])
-        filename = "%06d" % count + '.jpg'
+        filename = ("%06d" % count) + '.jpg'
         print(filename)
         # 保存圖片
         # urllib.request.urlretrieve(line.split("\t")[2], os.path.join(path , filename))
