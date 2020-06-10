5ade5a0a3c3c39e808761bc65baa263c7076a729
yuki23329626 <michael7105@gmail.com>
2020-06-10 14:57:53 +0800

    test

--- 3-hw/faceimg-downloader.py
+++ 3-hw/faceimg-downloader.py
@@ -1,4 +1,10 @@
-text_file = open("pubfig.txt", "r")
-lines = text_file.readlines()
-print(lines[2].split("\t")[3])
-text_file.close()
+
+# Public Figures Face Database(哥倫比亞大學公眾人物臉部數據庫)
+
+# 存圖位置
+path = 'data_pubfig/'
+
+with open('pubfig.txt') as object_pubfig:
+    for line in object_pubfig:
+      print(line.split("\t")[2])
+
