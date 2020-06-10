31f69980f2c54e73433288eb1fcbf69417071995
yuki23329626 <michael7105@gmail.com>
2020-06-10 15:05:43 +0800

    test

--- 3-hw/faceimg-downloader.py
+++ 3-hw/faceimg-downloader.py
@@ -1,12 +1,21 @@
 
+import urllib.request
+import os
+
 # Public Figures Face Database(哥倫比亞大學公眾人物臉部數據庫)
 
 # 存圖位置
 path = 'data_pubfig/'
 
+count = 1
 with open('pubfig.txt') as object_pubfig:
     for line in object_pubfig:
       li=line.strip()
       if not li.startswith("#"):
-        print(line.split("\t")[2])
+        #print(line.split("\t")[2])
+        filename = count + '.jpg'
+        print(filename)
+        # 保存圖片
+        # urllib.request.urlretrieve(line.split("\t")[2], os.path.join(path , filename))
+        count += 1
 
