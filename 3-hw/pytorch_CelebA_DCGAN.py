06c787a83e5af7820e3e67f6568e6f139571126e
yuki23329626 <michael7105@gmail.com>
2020-06-11 11:48:52 +0800

    test

--- 3-hw/pytorch_CelebA_DCGAN.py
+++ 3-hw/pytorch_CelebA_DCGAN.py
@@ -170,10 +170,10 @@ if (temp.shape[0] != img_size) or (temp.shape[0] != img_size):
     sys.stderr.write('Error! image size is not 64 x 64! run \"celebA_data_preprocess.py\" !!!')
     sys.exit(1)
 
-mu = np.zeros(64)
-sigma = np.eye(3)
+mu = 0
+sigma = 1
 size_normal_distribution = (64, 64, 4)
-noise = np.random.multivariate_normal(mu, sigma, size_normal_distribution)
+noise = np.random.normal(mu, sigma, size_normal_distribution)
 print(noise)
 
 temp = temp + noise
