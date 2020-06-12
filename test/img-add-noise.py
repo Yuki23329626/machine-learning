6ff407be0ba471041360c0f444f8581895f09fea
yukine23329626 <michael7105@gmail.com>
2020-06-12 16:48:32 +0800

    test

--- test/img-add-noise.py
+++ test/img-add-noise.py
@@ -6,8 +6,16 @@ img_path = "data/000001.png"
 img = plt.imread(img_path)
 mu = 0
 sigma = 1
-normal_distribution_noise = np.random.normal(mu, sigma, img.shape[:-1]) #  np.zeros((224, 224), np.float32)
+noise = np.random.normal(mu, sigma, img.shape[:-1]) #  np.zeros((224, 224), np.float32)
+denominator = np.max(normal_distribution_noise) - np.min(normal_distribution_noise)
 
-print(img)
-print(np.max(normal_distribution_noise))
-print(np.min(normal_distribution_noise))
+# print(img)
+# print(np.max(normal_distribution_noise))
+# print(np.min(normal_distribution_noise))
+
+noise_percent = noise/denominator
+
+fake_img = np.clip(img + noise_percent, 0, 1)
+
+plt.imshow(img, "origin", 1, 2, 1)
+plt.imshow(img, "normal distribution", 1, 2, 2)
