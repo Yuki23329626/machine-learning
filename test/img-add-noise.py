d482ab2962bb86cafa4a1ad619a787e7d85becae
yukine23329626 <michael7105@gmail.com>
2020-06-12 17:06:35 +0800

    test

--- test/img-add-noise.py
+++ test/img-add-noise.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 img_path = "data/000001.png"
 
 img = plt.imread(img_path)
-mu = 0
+mu = -10
 sigma = 1
 noise = np.random.normal(mu, sigma, img.shape) #  np.zeros((224, 224), np.float32)
 denominator = np.max(noise) - np.min(noise)
