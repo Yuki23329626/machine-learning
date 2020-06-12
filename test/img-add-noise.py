d604c18bdaa84b84c29d6430eee52d968be6eac4
yukine23329626 <michael7105@gmail.com>
2020-06-12 16:49:51 +0800

    test

--- test/img-add-noise.py
+++ test/img-add-noise.py
@@ -6,7 +6,7 @@ img_path = "data/000001.png"
 img = plt.imread(img_path)
 mu = 0
 sigma = 1
-noise = np.random.normal(mu, sigma, img.shape[:-1]) #  np.zeros((224, 224), np.float32)
+noise = np.random.normal(mu, sigma, img.shape) #  np.zeros((224, 224), np.float32)
 denominator = np.max(noise) - np.min(noise)
 
 # print(img)
