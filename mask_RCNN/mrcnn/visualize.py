20e1b864c8c840bb1147cf80d4ae39e7fb39f191
yuki23329626 <michael7105@gmail.com>
2020-06-15 04:40:31 +0800

    test

--- mask_RCNN/mrcnn/visualize.py
+++ mask_RCNN/mrcnn/visualize.py
@@ -73,7 +73,7 @@ def apply_mask(image, mask, color, alpha=0.5):
     """Apply the given mask to the image.
     """
     for c in range(3):
-        image[:, :, c] = np.where(mask == 0,
+        image[:, :, c] = np.where(mask == 0,# mask == 1
                                   image[:, :, c] *
                                   (1 - alpha) + alpha * color[c] * 255,
                                   image[:, :, c])
