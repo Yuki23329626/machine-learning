a08d454f519303b71c4cb46289baec6d105b19bb
yuki23329626 <michael7105@gmail.com>
2020-06-15 03:54:33 +0800

    test

--- mask_RCNN/mrcnn/visualize.py
+++ mask_RCNN/mrcnn/visualize.py
@@ -162,7 +162,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             # Subtract the padding and flip (y, x) to (x, y)
             verts = np.fliplr(verts) - 1
             p = Polygon(verts, facecolor="none", edgecolor=color)
-            ax.add_patch(p)
+            # ax.add_patch(p)
 
     ax.imshow(masked_image.astype(np.uint8))
     if auto_show:
