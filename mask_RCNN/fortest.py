966a5c3a453c1bde6662e842d2b2524dcbb0b20e
yuki23329626 <michael7105@gmail.com>
2020-06-15 03:11:01 +0800

    test

--- mask_RCNN/fortest.py
+++ mask_RCNN/fortest.py
@@ -105,4 +105,4 @@ b=datetime.now()
 # Visualize results
 print("shijian",(b-a).seconds)
 r = results[0]
-visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],class_names, r['scores'])
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],class_names, r['scores'], show_mask=False)
