7f4cd28bc33fbbf45d3771ca23c47f101b33ad4b
yuki23329626 <michael7105@gmail.com>
2020-06-16 05:50:43 +0800

    test

--- mask_RCNN/fortest.py
+++ mask_RCNN/fortest.py
@@ -97,11 +97,11 @@ class_names = ['BG', 'mango']
 # Load a random image from the images folder
 
 # 基础设置
-PATH_ORIGINAL_DATA = "../2-hw/datasets/C1-P1_Test/3/"
+PATH_ORIGINAL_DATA = "../2-hw/datasets/C1-P1_Train/2/"
 imglist = os.listdir(PATH_ORIGINAL_DATA)
 count = len(imglist)
 # print("count", count)
-PATH_OUTPUT_DATA = "data_mango_test/"
+PATH_OUTPUT_DATA = "data_mango/"
 
 # logger1.info('Just for testing')
 
