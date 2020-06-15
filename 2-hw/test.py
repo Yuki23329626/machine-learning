43da78eaabc77529ac1a51302138bbc5d905307b
yukine23329626 <michael7105@gmail.com>
2020-06-15 15:37:06 +0800

    test

--- 2-hw/test.py
+++ 2-hw/test.py
@@ -31,8 +31,8 @@ with torch.no_grad():
             data, target = data.cuda(), target.cuda()
 
         output = model(data)
-        print(output)
         loss = torch.nn.functional.cross_entropy(output, target)
+        print(loss)
         test_loss += loss.item() * data.size(0)
         correct += (output.max(1)[1] == target).sum()
         
