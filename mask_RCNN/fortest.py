3be097ad59ba5572593ac2d289ddc99b4d55f970
yuki23329626 <michael7105@gmail.com>
2020-06-16 04:49:44 +0800

    test

--- mask_RCNN/fortest.py
+++ mask_RCNN/fortest.py
@@ -28,7 +28,7 @@ from mrcnn import visualize
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
  
 # Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(MODEL_DIR ,"shapes20200615T0545/mask_rcnn_shapes_0010.h5")
+COCO_MODEL_PATH = os.path.join(MODEL_DIR ,"shapes20200615T1714/mask_rcnn_shapes_0010.h5")
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
