9a2a3482dcab563775dc20c5e94a17a63c9496bd
yuki23329626 <michael7105@gmail.com>
2020-06-17 02:52:50 +0800

    test

--- mask_RCNN/mrcnn/visualize.py
+++ mask_RCNN/mrcnn/visualize.py
@@ -209,7 +209,7 @@ def display_instances(image, boxes, masks, class_ids, class_names, filestr, path
     # resized_img = masked_image[x1:x2, y1:y2, :]
     # print(resized_img)
     masked_image = masked_image[x1:x2, y1:y2, :]
-    plt.show()
+    plt.show(masked_image)
     ax.imshow(masked_image.astype(np.uint8))
     # if auto_show:
     #     plt.show()
