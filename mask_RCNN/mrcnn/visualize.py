f61b9d94037da3ab4b48fc8205dd658342f9e23a
yuki23329626 <michael7105@gmail.com>
2020-06-17 01:40:53 +0800

    test

--- mask_RCNN/mrcnn/visualize.py
+++ mask_RCNN/mrcnn/visualize.py
@@ -142,7 +142,7 @@ def display_instances(image, boxes, masks, class_ids, class_names, filestr, path
 
     # Show area outside image boundaries.
     height, width = image.shape[:2]
-    ax.set_ylim(height + 1000, -10)
+    ax.set_ylim(height + 10, -10)
     ax.set_xlim(-10, width + 10)
     ax.axis('off')
     ax.set_title(title)
@@ -172,7 +172,7 @@ def display_instances(image, boxes, masks, class_ids, class_names, filestr, path
             continue
         y1, x1, y2, x2 = boxes[i]
         if show_bbox:
-            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+            p = patches.Rectangle((0, 0), x2 - x1, y2 - y1, linewidth=2,
                                 alpha=0.7, linestyle="dashed",
                                 edgecolor=color, facecolor='none')
             ax.add_patch(p)
