2ae790f86310a2aecdd7202cd17160a5636e0621
yukine23329626 <michael7105@gmail.com>
2020-06-22 18:17:34 +0800

    test

--- 3-hw/test.py
+++ 3-hw/test.py
@@ -11,9 +11,20 @@ from torchvision import datasets, transforms
 from torch.autograd import Variable
 import numpy as np
 
-mu = 0
-sigma = 1
-fixed_z_ = torch.normal(mu, sigma, (5 * 5, 100)).view(-1, 100, 1, 1)    # fixed noise
+# mu = 0
+# sigma = 1
+# fixed_z_ = torch.normal(mu, sigma, (5 * 5, 100)).view(-1, 100, 1, 1)    # fixed noise
 
-print ("fixed_z_:", fixed_z_)
-print ("fixed_z_.shape", fixed_z_.shape)
+# print ("fixed_z_:", fixed_z_)
+# print ("fixed_z_.shape", fixed_z_.shape)
+
+# 2020.06.22
+def find_dir(path):
+    # 函數功能: 遞迴顯示指定路徑下的所有檔案及資料夾名稱
+    for fd in os.listdir(path):
+        full_path=os.path.join(path,fd)
+        if os.path.isdir(full_path):
+            print('資料夾:',full_path)
+            find_dir(full_path)
+        else:
+            print('檔案:',full_path)
