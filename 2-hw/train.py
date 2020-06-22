55d4355f78ebc5a75aeb5c8be53971bf1b462e04
yukine23329626 <michael7105@gmail.com>
2020-06-22 17:52:25 +0800

    test 2hw train

--- 2-hw/train.py
+++ 2-hw/train.py
@@ -4,6 +4,55 @@ from datasets import make_train_loader
 
 import torch, os
 import numpy as np
+import matplotlib.pyplot as plt
+
+import logging
+
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='log.2-hw.train',
+                    filemode='w')
+
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger().addHandler(console)
+
+# Now, we can log to the root logger, or any other logger. First the root...
+# logging.info('just for test')
+
+# Now, define a couple of other loggers which might represent areas in your
+# application:
+
+logger1 = logging.getLogger('2-hw.train.py')
+
+def show_train_hist(training_loss, validation_loss,  show = False, save = False, path = 'hw3-2.png'):
+    x = range(len(training_loss))
+
+    plt.plot(x, training_loss, label='training_loss')
+    plt.plot(x, validation_loss, label='validation_loss')
+
+    plt.xlabel('Iter')
+    plt.ylabel('Loss')
+
+    plt.legend(loc=4)
+    plt.grid(True)
+    plt.tight_layout()
+
+    if save:
+        plt.savefig(path)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
 
 model = cnn_model()
 
@@ -21,6 +70,9 @@ if use_cuda:
 
 train_loader, valid_loader = make_train_loader(cfg)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+training_loss = []
+validation_loss = []
 
 for epoch in range(1, epochs+1):
     model.train()
@@ -50,6 +102,12 @@ for epoch in range(1, epochs+1):
     train_loss /= int(np.floor(len(train_loader.dataset) * (1 - valid_size)))
     valid_loss /= int(np.floor(len(valid_loader.dataset) * valid_size))
     print('Epoch: {}, Training Loss: {:.4f}, Validation Loss: {:.4f}'.format(epoch, train_loss, valid_loss))
+    logging.info("Training Loss: %s", train_loss)
+    logging.info("Validation Loss: %s", valid_loss)
+    training_loss = training_loss.extend(train_loss)
+    validation_loss = validation_loss.extend(valid_loss)
+
+show_train_hist(training_loss, validation_loss, save=True)
 
 output_dir = "/".join(weight_path.split("/")[:-1])
 if not os.path.exists(output_dir):
